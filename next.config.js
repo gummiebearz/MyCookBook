993ac0dc646249cfe78f79ed5b00d62c0d63cafd
Vivian Pham <gumball@OldAdventureTime.local>
2022-09-24 20:04:34 -0400

    Nextjs configs

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false, // prevent rendering twice in dev mode
+    swcMinify: true
+}
+
+module.exports = nextConfig
