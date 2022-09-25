31a4393142a6a158dff2b1e74154563716339518
Vivian Pham <gumball@OldAdventureTime.local>
2022-09-24 20:12:13 -0400

    Homepage

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Navbar, Menu } from '../components'
+
+export default function Home() {
+    return (
+        <div>
+            <main>
+                <Navbar />
+                <Menu />
+            </main>
+        </div>
+    )
+}
